ff97b8030fbf5b8d153911107b438f095b93e6cc
DeDelner <dedelner@outlook.com>
2023-05-08 08:26:03 +0200

    Initial Commit

---
+++ MusicSync/MusicSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSyncApp.swift
+//  MusicSync
+//
+//  Created by Christian Langolf on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
